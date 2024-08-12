c418020ed2d45c952e68110675bef94f99035077
Mohd-Murtaza <mohd.murtaza0101@gmail.com>
2024-08-13 02:30:52 +0530

    add some logs on craete blogs route

--- routes/blogsRoutes.js
+++ routes/blogsRoutes.js
@@ -6,11 +6,12 @@ const {auth} = require('../middlewares/authMiddleware');
 // Create Blog
 router.post('/', auth, async (req, res) => {
     const { title, description } = req.body;
+    console.log("on line 9----",req.body.userId,req.body.name)
 
     try {
         const user = await User.findById(req.user.userId);
         if (!user) return res.status(404).json({ message: 'User not found' });
-
+        console.log("user on line no 13",user)
         const newBlog = new Blog({
             title,
             description,
@@ -20,7 +21,7 @@ router.post('/', auth, async (req, res) => {
         await newBlog.save();
         res.status(201).json(newBlog);
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        res.status(400).json({ message: error.message,msg:"error while creating a blog" });
     }
 });
 
